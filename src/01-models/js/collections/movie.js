529146d34ff9bb62406524aace7688b70601c030
Alexandros David <adavid@tripsta.com>
2015-05-27 11:10:32 +0300

    exercise1-models

---
+++ src/01-models/js/collections/movie.js
@@ -0,0 +1,45 @@
+var collectionMovies = Backbone.Collection.extend ({
+	
+	initialize: function() {
+		console.log("collection init");
+	}
+	
+});
+
+var collectionMovie = new collectionMovies();
+
+collectionMovie.reset([movie1, movie2, movie3, movie4, movie5]);
+
+console.log(collectionMovie);
+
+$("#content").html(JSON.stringify(collectionMovie));
+
+
+
+
+$("#addModel").click(function() {
+
+	var randomNumber = Math.floor((Math.random() * 10) % 5);
+	
+	console.log(randomNumber);
+	
+	//console.log(arrayMovies[randomNumber]);
+	
+
+	collectionMovie.add(arrayMovies[randomNumber]);
+	
+	console.log(collectionMovie);
+	
+	$("#content").html(JSON.stringify(collectionMovie));
+});
+
+
+
+var arrayMovies = []
+
+arrayMovies[0] = movie6;
+arrayMovies[1] = movie7;
+arrayMovies[2] = movie8;
+arrayMovies[3] = movie9;
+arrayMovies[4] = movie10;
+
---
+++ src/01-models/js/models/movie.js
@@ -0,0 +1,126 @@
+var movieModel = Backbone.Model.extend ({ 
+	
+	initialize: function() {
+		console.log("movie init");
+	}
+	
+});
+
+var movie1 = new movieModel({
+ 	
+	title:"Pulp Fiction",
+	year:"1994",
+	genre:"Crime, Drama",
+	image:"http://ia.media-imdb.com/images/M/MV5BMjE0ODk2NjczOV5BMl5BanBnXkFtZTYwNDQ0NDg4._V1_SY317_CR4,0,214,317_AL_.jpg",
+	director:"Quentin Tarantino"
+});
+
+
+
+var movie2 = new movieModel({
+ 	
+	title:"Fight Club",
+	year:"1999",
+	genre:"Drama",
+	image:"http://ia.media-imdb.com/images/M/MV5BMjIwNTYzMzE1M15BMl5BanBnXkFtZTcwOTE5Mzg3OA@@._V1_SX214_AL_.jpg",
+	director:"David Fincher"
+});
+
+var movie3 = new movieModel({
+ 	
+	title:"Forrest Gump",
+	year:"1994",
+	genre:"Drama, Romance",
+	image:"http://ia.media-imdb.com/images/M/MV5BMTQwMTA5MzI1MF5BMl5BanBnXkFtZTcwMzY5Mzg3OA@@._V1_SX214_AL_.jpg",
+	director:"Robert Zemeckis"
+});
+
+
+var movie4 = new movieModel({
+ 	
+	title:"Inception",
+	year:"2010",
+	genre:" Action, Mystery, Sci-Fi",
+	image:"http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX214_AL_.jpg",
+	director:"Christopher Nolan"
+});
+
+var movie5 = new movieModel({
+ 	
+	title:"The Matrix",
+	year:"1999",
+	genre:"Action, Sci-Fi",
+	image:"http://ia.media-imdb.com/images/M/MV5BMTkxNDYxOTA4M15BMl5BanBnXkFtZTgwNTk0NzQxMTE@._V1_SX214_AL_.jpg",
+	director:" Andy Wachowski"
+});
+
+
+
+var movie6 = new movieModel({
+ 	
+	title:"Seven",
+	year:"1995",
+	genre:"Drama, Mystery, Thriller",
+	image:"http://ia.media-imdb.com/images/M/MV5BMTQwNTU3MTE4NF5BMl5BanBnXkFtZTcwOTgxNDM2Mg@@._V1_SX214_AL_.jpg",
+	director:"David Fincher"
+});
+
+
+var movie7 = new movieModel({
+ 	
+	title:"Léon ",
+	year:"1994",
+	genre:"Crime, Drama, Thriller",
+	image:"http://ia.media-imdb.com/images/M/MV5BMTgzMzg4MDkwNF5BMl5BanBnXkFtZTcwODAwNDg3OA@@._V1_SY317_CR4,0,214,317_AL_.jpg",
+	director:"Luc Besson"
+});
+
+var movie8 = new movieModel({
+ 	
+	title:"InterStellar",
+	year:"2014",
+	genre:"Adventure, Sci-Fi",
+	image:"http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX214_AL_.jpg",
+	director:"Christopher Nolan"
+});
+
+
+var movie9 = new movieModel({
+ 	
+	title:"Memento",
+	year:"2000",
+	genre:"Mystery, Thriller",
+	image:"http://ia.media-imdb.com/images/M/MV5BMTc4MjUxNDAwN15BMl5BanBnXkFtZTcwMDMwNDg3OA@@._V1_SY317_CR12,0,214,317_AL_.jpg",
+	director:"Christopher Nolan"
+});
+
+var movie10 = new movieModel({
+ 	
+	title:"The Prestige",
+	year:"2006",
+	genre:"Drama, Mystery, Thriller",
+	image:"http://ia.media-imdb.com/images/M/MV5BMjA4NDI0MTIxNF5BMl5BanBnXkFtZTYwNTM0MzY2._V1_SX214_AL_.jpg",
+	director:"Christopher Nolan"
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(movie1);
+
+
+
+
+
+
+
+
